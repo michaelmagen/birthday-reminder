c4f1beac0e9265ad0651552563d1ef19f126ae57
Michael Magen <michael.magen101@gmail.com>
2022-12-29 11:40:05 -0800

    Add display date selector

--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -14,13 +14,35 @@ import {
   Paper,
   Container,
   Flex,
+  createStyles,
 } from '@mantine/core'
+import { useMediaQuery } from '@mantine/hooks'
+import { DatePicker } from '@mantine/dates'
+import dayjs from 'dayjs'
+import { IconCalendar } from '@tabler/icons'
 import data from '../config/data'
+
+const useStyles = createStyles((theme) => ({
+  outside: {
+    opacity: 0,
+  },
+
+  weekend: {
+    color:
+      theme.colorScheme === 'light'
+        ? `${theme.colors.gray[7]} !important`
+        : `${theme.colors.dark[0]} !important`,
+  },
+}))
 
 export default function Home() {
   const navigate = useNavigate()
   const { session, loadingSession } = useAuth()
   const [formLoading, setFormLoading] = useState(false)
+  const [displayDate, setDisplayDate] = useState(new Date())
+  console.log(displayDate)
+  const isMobile = useMediaQuery('(max-width: 755px)')
+  const { classes, cx } = useStyles()
 
   // if user not logged in, send them to login page
   useEffect(() => {
@@ -104,6 +126,26 @@ export default function Home() {
             </Center>
           </form>
         </Paper>
+        <Flex justify="center" align="center">
+          <DatePicker
+            dropdownType={isMobile ? 'modal' : 'popover'}
+            placeholder="Pick date"
+            label="Select date"
+            firstDayOfWeek="sunday"
+            minDate={dayjs(new Date()).startOf('year').toDate()}
+            maxDate={dayjs(new Date()).endOf('year').toDate()}
+            dayClassName={(date, modifiers) =>
+              cx({
+                [classes.outside]: modifiers.outside,
+                [classes.weekend]: modifiers.weekend,
+              })
+            }
+            styles={{ root: { width: '15rem' } }}
+            icon={<IconCalendar size={16} />}
+            value={displayDate}
+            onChange={setDisplayDate}
+          />
+        </Flex>
       </Container>
     </>
   )
