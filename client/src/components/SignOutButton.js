d7b22eee69fd30f7ac038af01165228d2e5fc9db
Michael Magen <michael.magen101@gmail.com>
2023-01-11 11:54:41 -0800

    Add loading to signout button

--- client/src/components/SignOutButton.js
+++ client/src/components/SignOutButton.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Button } from '@mantine/core'
 import { useAuth } from '../context/AuthContext'
 import { useNavigate } from 'react-router-dom'
@@ -5,16 +6,19 @@ import { IconLogout } from '@tabler/icons'
 
 export default function SignOutButton() {
   const { logout } = useAuth()
+  const [loading, setLoading] = useState(false)
   const navigate = useNavigate()
 
   // log user out of app and navigate to login page
   async function handleLogout() {
     try {
+      setLoading(true)
       await logout()
       navigate('/login')
     } catch (e) {
       console.log(e)
     }
+    setLoading(false)
   }
 
   return (
@@ -23,6 +27,7 @@ export default function SignOutButton() {
       variant="light"
       color="red"
       radius="md"
+      loading={loading}
       leftIcon={<IconLogout />}
     >
       Sign out
